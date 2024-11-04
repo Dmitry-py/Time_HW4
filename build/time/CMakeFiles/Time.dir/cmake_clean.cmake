e23f8d4e8785987a4a5d67a0b4e54c38586bd052
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-11-04 17:45:13 +0700

    Time repo creation

---
+++ build/time/CMakeFiles/Time.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Time.dir/time.cpp.o"
+  "CMakeFiles/Time.dir/time.cpp.o.d"
+  "libTime.a"
+  "libTime.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Time.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
